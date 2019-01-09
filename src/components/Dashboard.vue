6ddfb8b36d12f67e5c48ed44754fb630f00b7b1b
joshparkerj <superfakek@gmail.com>
2019-01-08 21:01:30 -0700

    get started again

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dashboard">
+    <h1>{{ msg }}</h1>
+    <Product msg="Welcome to Product"/>
+  </div>
+</template>
+
+<script>
+import Product from './Product';
+
+export default {
+  name: 'Dashboard',
+  props: {
+    msg: String
+  },
+  components: {
+    Product
+  }
+}
+</script>
+
+<style>
+.dashboard {
+  background-color: chartreuse;
+  margin: 10%;
+  padding: 10%;
+  border: 1px solid;
+}
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="product">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    msg: String
+  },
+}
+</script>
+
+<style>
+.product {
+  background-color: yellow;
+  margin: 2%;
+  padding: 2%;
+  border: 1px solid;
+  border-radius: 2%;
+}
+</style>
